f026b6747ab0039491cd46fb30d68dd357055091
Leonardo <leohuemaster@gmail.com>
2024-03-27 14:49:49 -0300

    Adicionar arquivos de projeto.

---
+++ MAUIParaSerTestado/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿#if NET8_0
+
+#else
+    using CommunityToolkit.Maui; 
+#endif
+
+using Microsoft.Extensions.Logging;
+
+namespace MAUIParaSerTestado
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+#if NET8_0
+
+#else
+                .UseMauiCommunityToolkit()
+#endif
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ MSTestMAUI/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MSTestMAUI
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
